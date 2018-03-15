25fb0b554f1ef4b7198b9b54b54e4fdb62f2cc92
Mika Väisänen <criticalpug@gmail.com>
2018-03-15 18:01:54 +0200

    Map added

---
+++ src/components/Map.vue
@@ -0,0 +1,41 @@
+
+
+<template>
+  <div class="container">
+    <div class="map" id="map">
+      <v-map :zoom=13 :center="[61.49911, 23.78712]">
+        <v-tilelayer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></v-tilelayer>
+        <v-marker :lat-lng="[47.413220, -1.219482]"></v-marker>
+      </v-map>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Map',
+  data() {
+    return {
+      msg: 'Here be maps',
+    };
+  },
+};
+</script>
+
+<style>
+  @import "../../node_modules/leaflet/dist/leaflet.css";
+</style>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .container {
+    display: flex;
+    justify-content: center;
+    align-items:center;
+  }
+  #map {
+    height: 50vh;
+    width: 50vw;
+  }
+</style>
