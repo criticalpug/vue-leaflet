8f77bd1525291c18a985dc32d53ac9fd4f2e31f5
Mika Väisänen <criticalpug@gmail.com>
2018-03-18 19:09:39 +0200

    All ships in test data rendered

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -3,24 +3,71 @@
     <div class="map" id="map">
       <v-map :zoom="zoom" :center="center">
         <v-tilelayer :url="url"></v-tilelayer>
-        <v-ais v-for="ship in ships"
+        <v-ais v-for="(ship) in ships"
           :key="ship.id"
           :lat-lng="ship.position"
           :visible="ship.visible"
           :options="ship.options">
           <v-popup :content="ship.tooltip"></v-popup>
-          <!-- <v-tooltip :content="ship.tooltip"></v-tooltip> -->
         </v-ais>
+        <v-poly v-for="(ship) in ships" :key="`route-`+ ship.id" :lat-lngs="ship.route"></v-poly>
       </v-map>
     </div>
-
   </div>
 </template>
 
 <script>
 import Vue2Leaflet from 'vue2-leaflet';
 import Vue2LeafletTracksymbol from 'vue2-leaflet-tracksymbol';
+import * as R from 'ramda';
+
 import MarkerPopup from './MarkerPopup';
+
+const AIS = require('../data/ais.json');
+
+// const Kiisla = 230956000;
+
+const degreesToRadians = degrees => Math.round(degrees * (Math.PI / 180));
+const knotsInMS = knots => knots * 0.514444444;
+
+const getOptions = ais => ({
+  trackId: ais.mmsi,
+  fill: true,
+  fillColor: '#00ffff',
+  fillOpacity: 1.0,
+  stroke: true,
+  color: '#000000',
+  opacity: 1.0,
+  weight: 1.0,
+  speed: knotsInMS(ais.sog), // meter per second
+  course: degreesToRadians(ais.cog), // radians
+  heading: degreesToRadians(ais.heading), // radians
+  size: 24,
+  defaultSymbol: [0.75, 0, 0.5, 0.3, -0.5, 0.3, -0.25, 0, -0.5, -0.3, 0.5, -0.3],
+  data: { name: 'Boat 2', custom: 'other info' },
+});
+
+/* eslint-disable no-param-reassign */
+const accumulator = (obj, ais) => {
+  if (!obj.id) {
+    obj.id = ais.mmsi;
+    obj.route = [];
+    obj.tooltip = `MMSI: ${ais.mmsi} \n lat: ${ais.lat} lon: ${ais.lon}`;
+    obj.draggable = false;
+    obj.visible = true;
+    obj.options = getOptions(ais);
+  }
+
+  obj.position = { lat: ais.lat, lng: ais.lon };
+  obj.route.push({ lng: ais.lon, ...ais });
+
+  return obj;
+};
+
+const groupedAIS = R.pipe(
+  R.groupBy(R.prop('mmsi')),
+  R.map(list => R.reduce(accumulator, {}, list)),
+)(AIS);
 
 export default {
   name: 'Map',
@@ -38,36 +85,10 @@ export default {
   data() {
     return {
       zoom: 6,
-      marker: [63.027087, 21.095948],
       center: [63.027087, 21.095948],
-      title: 'title',
-      text: 'text',
       url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
-      ships: [
-        {
-          id: '230662000',
-          position: { lat: 63.11851333333333, lng: 20.124348333333334 },
-          tooltip: 'MMSI: 230662000',
-          draggable: true,
-          visible: true,
-          options: {
-            trackId: 123,
-            fill: true,
-            fillColor: '#00ffff',
-            fillOpacity: 1.0,
-            stroke: true,
-            color: '#000000',
-            opacity: 1.0,
-            weight: 1.0,
-            speed: 30, // meter per second
-            course: 1.0, // radians
-            heading: 1.0, // radians
-            size: 24,
-            defaultSymbol: [0.75, 0, 0.5, 0.3, -0.5, 0.3, -0.25, 0, -0.5, -0.3, 0.5, -0.3],
-            data: { name: 'Boat 2', custom: 'other info' },
-          },
-        },
-      ],
+      ships: [],
+      route: [],
     };
   },
   // methods: {
@@ -76,7 +97,15 @@ export default {
   //   },
   // },
   mounted() {
+    /* eslint-disable no-console */
+    console.log('Mounted');
 
+    // this.ships = [AISKiisla.map(ais => convertAisToShip(ais))[AISKiisla.length - 1]];
+    this.ships = groupedAIS;
+  },
+  created() {
+    /* eslint-disable no-console */
+    console.log('Created');
   },
 };
 </script>
